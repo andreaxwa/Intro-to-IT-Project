d20ac42944a2bd29bcf14d13d3c137cd048bce68
Nguyen Nghia Cuong <s3998180@rmit.edu.vn>
2023-06-02 23:09:37 +0700

    Update references, timeframe

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Smooth scrolling to section
 function scrollToSection(id) {
     const section = document.getElementById(id);
-    const offset = 140;
+    const offset = 130;
     
     if (section) {
       const top = section.offsetTop - offset;
@@ -11,36 +11,36 @@ function scrollToSection(id) {
       });
     }
   }
-  
-  // Add click event listeners to navigation links
-  document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav a');
+
+// Click navigation menu items to scroll to section
+document.addEventListener('DOMContentLoaded', function() {
+const navLinks = document.querySelectorAll('nav a');
+
+for (let i = 0; i < navLinks.length; i++) {
+    let link = navLinks[i];
     
-    for (let i = 0; i < navLinks.length; i++) {
-      let link = navLinks[i];
-      
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        scrollToSection(targetId);
-      });
-    }
-  });
-  
-  // Back to top button functionality
-  window.addEventListener('scroll', function() {
-    const backToTopButton = document.getElementById('back-to-top');
-    if (window.scrollY > 200) {
-      backToTopButton.style.display = 'block';
-    } else {
-      backToTopButton.style.display = 'none';
-    }
-  });
-  
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+    link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    scrollToSection(targetId);
     });
-  }
-  
+}
+});
+
+// Reveal go to top button
+window.addEventListener('scroll', function() {
+const goToTopButton = document.getElementById('go-to-top');
+if (window.scrollY > 200) {
+    goToTopButton.style.display = 'block';
+} else {
+    goToTopButton.style.display = 'none';
+}
+});
+
+// Scroll to top when clicked
+function scrollToTop() {
+window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+}
